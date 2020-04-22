5f1888958246008b418638793e203a94db23631a
princejoogie <princejoogie@gmail.com>
2020-04-23 01:03:20 +0800

    PUI

--- lib/questions/pui_screen.dart
+++ lib/questions/pui_screen.dart
@@ -8,52 +8,89 @@ class PUIScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-            children: [
-              SizedBox(height: 50.0),
-              Text("PUI Screen"),
-              SizedBox(height: 50.0),
+            children: <Widget>[
               Container(
-                width: 250,
-                height: 60,
-                child: OutlineButton(
-                  child: Text(
-                    "Malala / Kritikal",
-                    style: TextStyle(
-                      color: Color(0xFF3f9d2f),
+                height: MediaQuery.of(context).size.height / 2 - 50,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    Text(
+                      "PUI",
+                      style: TextStyle(
+                        fontSize: 100,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFab0800),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).pushReplacementNamed('/AdmitScreen');
-                  },
-                  borderSide: BorderSide(
-                    color: Color(0xFF3f9d2f),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
+                    Text("Patient Under Investigation",
+                        style: TextStyle(
+                          color: Color(0xFFab0800),
+                        )),
+                  ],
                 ),
               ),
-              SizedBox(height: 50.0),
               Container(
-                width: 250,
-                height: 60,
-                child: OutlineButton(
-                  child: Text(
-                    "Di Gaano / Mild",
-                    style: TextStyle(
-                      color: Color(0xFF3f9d2f),
+                height: MediaQuery.of(context).size.height / 2 - 50,
+                child: ListView(
+                  reverse: true,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 250,
+                        height: 60,
+                        child: OutlineButton(
+                          child: Text(
+                            "Di Gaano / Mild",
+                            style: TextStyle(
+                              color: Color(0xFF3f9d2f),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacementNamed('/AdultChildScreen');
+                          },
+                          borderSide: BorderSide(
+                            color: Color(0xFF3f9d2f),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .pushReplacementNamed('/AdultChildScreen');
-                  },
-                  borderSide: BorderSide(
-                    color: Color(0xFF3f9d2f),
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
+                    SizedBox(height: 10.0),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 250,
+                        height: 60,
+                        child: OutlineButton(
+                          child: Text(
+                            "Malala / Kritikal",
+                            style: TextStyle(
+                              color: Color(0xFF3f9d2f),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacementNamed('/AdmitScreen');
+                          },
+                          borderSide: BorderSide(
+                            color: Color(0xFF54c242),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text("Uri ng Sintomas?"),
+                    ),
+                  ],
                 ),
               ),
             ],
